6b2bd617bd585efc778ba9b4dce5bd669d608b92
Raghav Goyal <12301raghavgoyal@gmail.com>
2022-02-12 20:22:03 +0530

    designed button and input text field

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
+import {useState} from "react";
 
 export default function Home() {
+	const [req, setReq] = useState(false);
 	return (
 		<div>
 			<Head>
@@ -12,6 +14,65 @@ export default function Home() {
 				/>
 				{/* <link rel="icon" href="/favicon.ico" /> */}
 			</Head>
+			<form
+				className=" form-control mx-auto flex max-w-sm flex-col justify-center space-y-10 py-16 accent-os-500"
+				onSubmit={e => {
+					e.preventDefault();
+					setReq(true);
+				}}>
+				<div className="flex flex-col space-y-2">
+					<div className="flex flex-col-reverse">
+						<input
+							name="textField1"
+							type="text"
+							placeholder="Type Something here ..."
+							className="input-text peer"
+							autoComplete="off"
+							required={req}
+						/>
+						<span className="peer-input-text">{"|"}</span>
+					</div>
+					<div className="flex flex-col-reverse">
+						<input
+							name="textField1"
+							type="text"
+							placeholder="Type Something here ..."
+							className="input-text  peer"
+							autoComplete="off"
+						/>
+						<span className="peer-input-text">{"|"}</span>
+					</div>
+					<div className="flex flex-col-reverse">
+						<input
+							name="textField1"
+							type="email"
+							placeholder="Type Something here ..."
+							className="input-text peer"
+							autoComplete="off"
+						/>
+						<span className="peer-input-text">{"|"}</span>
+					</div>
+					<div className="flex flex-col-reverse">
+						<input
+							name="textField1"
+							type="email"
+							placeholder="Type Something here ..."
+							className="input-text peer"
+							autoComplete="off"
+							required={req}
+						/>
+						<span className="peer-input-text">{"|"}</span>
+					</div>
+				</div>
+				<div className="flex items-center space-x-2">
+					<input id="abc" name="checkbox1" type="checkbox" className="text-lg" />
+					<label htmlFor="abc">sacac</label>
+				</div>
+				<button className="button-od" type="Submit">
+					OpenDesk
+				</button>
+				<button className="button-os">OpenShelf</button>
+			</form>
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
 	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {}
+		extend: {},
+		colors: {
+			os: colors.emerald,
+			od: colors.violet,
+			black: colors.black,
+			white: colors.white,
+			gray: colors.gray,
+			blue: colors.blue,
+			red: colors.red
+		}
+	},
+	variants: {
+		extend: {
+			fontWeight: ["hover"]
+		}
 	},
 	plugins: [
 		require("@tailwindcss/typography"),
-		require("@tailwindcss/forms"),
 		require("@tailwindcss/line-clamp"),
 		require("@tailwindcss/aspect-ratio")
 	]
