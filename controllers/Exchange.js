22d893f51cf536886914a2ac99f29a30c0fa85dc
Nonit Mittal <nonitmittal@gmail.com>
2022-03-19 21:28:01 +0530

    - fixed bugs - add await and err func wherever required - add offerPrice value in MakeOffer call

--- controllers/Exchange.js
+++ controllers/Exchange.js
@@ -2,37 +2,58 @@ import {ethers} from "ethers";
 import addresses from "../contracts/addresses.json";
 import Exchange from "../contracts/abis/Exchange.json";
 
-function callContract(signer, functionToCall) {
+async function callContract(signer, functionToCall, handleError) {
     const contract = new ethers.Contract(addresses.exchange, Exchange.abi, signer);
     try {
-        return functionToCall(contract);
+        return await functionToCall(contract);
     } catch (error) {
-        console.error(error);
+        await handleError(error);
     }
 }
 
 export async function makeOffer(signer, bookAddress, copyUid, offerPrice) {
-    callContract(signer, async contract => {
-        const transaction = await contract.makeOffer(bookAddress, copyUid, offerPrice);
-        const transactionStatus = await transaction.wait();
-        console.log(transactionStatus);
-    });
+    offerPrice = ethers.utils.parseUnits(offerPrice.toString(), "ether");
+    await callContract(
+        signer,
+        async contract => {
+            const transaction = await contract.makeOffer(bookAddress, copyUid, offerPrice, {
+                value: offerPrice
+            });
+            const transactionStatus = await transaction.wait();
+            console.log(transactionStatus);
+        },
+        err => {
+            console.log(err);
+        }
+    );
 }
 
 export async function cancelOffer(signer, bookAddress, copyUid) {
-    callContract(signer, async contract => {
-        const transaction = await contract.cancelOffer(bookAddress, copyUid);
-        const transactionStatus = await transaction.wait();
-        console.log(transactionStatus);
-    });
+    await callContract(
+        signer,
+        async contract => {
+            const transaction = await contract.cancelOffer(bookAddress, copyUid);
+            const transactionStatus = await transaction.wait();
+            console.log(transactionStatus);
+        },
+        err => {
+            console.log(err);
+        }
+    );
 }
 
 export async function offerAccepted(signer, bookAddress, copyUid, buyer) {
-    callContract(signer, async contract => {
-        const transaction = await contract.offerAccepted(bookAddress, copyUid, buyer);
-        const transactionStatus = await transaction.wait();
-        console.log(transactionStatus);
-    });
+    await callContract(
+        signer,
+        async contract => {
+            const transaction = await contract.offerAccepted(bookAddress, copyUid, buyer);
+            const transactionStatus = await transaction.wait();
+            console.log(transactionStatus);
+        },
+        err => {
+            console.log(err);
+        }
+    );
 }
 
 export function contractAbi() {
